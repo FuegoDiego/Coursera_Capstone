b5d1093a72f6145e55fba805b5aa2594dd6bff13
Diego <diego.caballero14@gmail.com>
2020-06-24 14:26:35 -0400

    Loaded the BIA dataset and cleaned the venue dataset.

--- parallelvenues.py
+++ parallelvenues.py
@@ -1,4 +1,3 @@
-import requests  # handle requests
 import json
 import numpy as np
 import concurrent.futures as cf
@@ -6,12 +5,14 @@ import time
 import itertools
 import pandas as pd
 import geopandas
+import sys
+import requests
 from shapely.geometry import Point
 
 with open("./params.json", "r") as f:
     params = json.load(f)
 
-def GetVenuesForCoord(nbh_name, latitude, longitude, radius, limit):
+def GetVenuesForCoord(latitude, longitude, radius, limit):
     CLIENT_ID = params['api']['client_id']
     CLIENT_SECRET = params['api']['client_secret']
     VERSION = params['api']['version']
@@ -30,51 +31,40 @@ def GetVenuesForCoord(nbh_name, latitude, longitude, radius, limit):
     try:
         req = requests.get(url).json()
     except Exception as e:
-        return [(nbh_name, str(e), 'quota_exceeded', np.nan, 'error')]
+        return [(str(e), 'quota_exceeded', np.nan, 'error')]
     
     try:
         venues = req['response']['venues']
     except:
-        return [(nbh_name, req, 'error', np.nan, 'error')]
+        return [(req, 'error', np.nan, 'error')]
 
     venue_list = [(
-        nbh_name,
         v['id'],
         v['name'],
         Point(v['location']['lng'], v['location']['lat']),
         v['categories'][0]['name']
     ) for v in venues]
+    
+    if venue_list == []:
+        return [(None, str(latitude), np.nan, str(longitude))]
 
     return venue_list
 
-def GetVenuesForNbh(nbh_name, latitudes, longitudes, radius, max_threads, limit=100):
+def GetVenuesForNbh(latitudes, longitudes, radius, max_threads, limit=100):    
     start = time.time()
 
-    nbh_names = []
-    lats = []
-    lngs = []
-    radii = []
-    limits = []
-    for nbh, lat, lng, r in zip(nbh_name, latitudes, longitudes, radius):
-        nbh_names.extend([nbh] * len(lat))
-        lats.extend(lat)
-        lngs.extend(lng)
-        radii.extend([r] * len(lat))
-        limits.extend([limit] * len(lat))
+    radii = [radius] * len(latitudes)
+    limits = [limit] * len(latitudes)
     
     # parallelize API calls to improve run time
     with cf.ThreadPoolExecutor(max_workers=max_threads) as pool:
-        venue_lst = list(pool.map(GetVenuesForCoord, nbh_names, lats, lngs, radii, limits))
+        venue_lst = list(pool.map(GetVenuesForCoord, latitudes, longitudes, radii, limits))
         
     venues_df = geopandas.GeoDataFrame([item for venue_lst in venue_lst for item in venue_lst])
 
-    venues_df.columns = ['Neighbourhood',
-                         'Venue ID',
-                         'Venue', 
-                         'geometry', 
-                         'Venue Category']
+    venues_df.columns = ['Venue ID', 'Venue', 'geometry', 'Venue Category']
     
     end = time.time()
-    print('Total time for neighbourhood ', nbh_name, ':', end - start, '\n')
+    print('Total time:', end - start, '\n')
 
     return venues_df
